04060a5de1546d36be6c0bfa1404b94d1be2e045
Rishil Boddula <rishilboddula@gmail.com>
2025-12-17 16:39:35 +0000

    Airflow successfully training and logging modles in mlflow

--- dags/training_pipeline.py
+++ dags/training_pipeline.py
@@ -1,7 +1,7 @@
 from data_ingestion import ingest_csv
 from data_cleaner import data_cleaning
 from airflow.decorators import task,dag
-
+from model_trainer import model_training
 
 
 
@@ -15,6 +15,7 @@ def training_pipeline():
     csv_path='/opt/airflow/Dataset_Folder/fake_job_postings.csv'
     task_ingestion=ingest_csv(csv_path)
     task_cleaning= data_cleaning()
-    task_ingestion>>task_cleaning
+    task_training=model_training()
+    task_ingestion>>task_cleaning>>task_training
 
 pipeline=training_pipeline()
--- src/helper_trainer.py
+++ src/helper_trainer.py
@@ -2,6 +2,8 @@ import logging
 from abc import ABC,abstractmethod
 import numpy as np
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 
 class Model_Training(ABC):
@@ -13,17 +15,23 @@ class Model_Training(ABC):
 class Decision_Trees(Model_Training):
     def train_model(self,x_test:np.ndarray,y_test:pd.DataFrame,**kwargs): 
         try:
-            #training model
+            dtc_model=DecisionTreeClassifier(**kwargs)
+            training_model=dtc_model.fit(x_test,y_test)
             logging.info('Decision Tree model is succcesssfully trained')
+            return training_model
         except Exception as e:
-            logging.error(f'Error in training the deciasion tree classifier')
+            logging.error(f'Error in training the deciasion tree classifier:{e}')
+            raise e
 class KNN_classifier(Model_Training):
     def train_model(self,x_test:np.ndarray,y_test:pd.DataFrame,**kwargs): 
         try:
-            #training model
+            knn_model=KNeighborsClassifier(**kwargs)
+            training_model=knn_model.fit(x_test,y_test)
             logging.info('KNN classifier model is succcesssfully trained')
+            return training_model
         except Exception as e:
-            logging.error(f'Error in training the KNN classifier classifier')
+            logging.error(f'Error in training the KNN classifier classifier:{e}')
+            raise e
 
 class XGBOOST_classifier(Model_Training):
     def train_model(self,x_test:np.ndarray,y_test:pd.DataFrame,**kwargs): 
